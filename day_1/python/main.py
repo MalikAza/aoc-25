a75623973c9f78af2796fbe8ddd04cb15763a6a0
Dakota <dakota742009@gmail.com>
2025-12-01 22:33:26 +0100

    feat: D1P2

--- day_1/python/main.py
+++ day_1/python/main.py
@@ -1,53 +1,77 @@
 from functools import cache
 from io import TextIOWrapper
-from itertools import count
 import re
-from typing import List
+from typing import List, Literal, TypedDict
 from utils.python import solution_print
 from utils.python.input import get_input_file_from_script_file
 
+class ParsedRotation(TypedDict):
+    rotation: Literal['L', 'R']
+    tick: int
+    result: int
+
 class Solver:
+    pass_by_zero_count: int = 0
     dial: int = 50
+    parsed: bool = False
     rotations: List[str]
 
     def __init__(self, file: TextIOWrapper):
         self.rotations = [line.replace('\n', '') for line in file.readlines()]
 
-    @cache
-    def __parse_rotation(self, rotation: str) -> int:
+    def __parse_rotation(self, rotation: str) -> ParsedRotation:
         pattern = r'^(?P<rotation>[LR])(?P<tick>\d+)$'
         matching = re.match(pattern, rotation)
 
         if matching:
-            rotation = matching.group('rotation')
+            rotation: Literal['L', 'R'] = matching.group('rotation')
             tick = int(matching.group('tick'))
+            result = -tick if rotation == 'L' else tick
 
-        if rotation == 'L':
-            return - tick
-        return tick
+            return {
+                'rotation': rotation,
+                'tick': tick,
+                'result': result
+            }
 
     def __calculate_future_dial_value(self, rotation: str) -> int:
-        future = self.dial + self.__parse_rotation(rotation)
-        if 0 < future < 99:
-            self.dial = future
-        else:
-            self.dial = future % 100
+        change = self.__parse_rotation(rotation)
+        future = self.dial + change.get('result')
+
+        match change.get('rotation'):
+            case 'L':
+                self.pass_by_zero_count += ((self.dial - 1) // 100) - ((future - 1) // 100)
+            case 'R':
+                self.pass_by_zero_count += (future // 100) - (self.dial // 100)
+        
+        self.dial = future % 100
 
         return self.dial
-        
-    def solve(self):
-        tick_numbers = [
+    
+    def __parse(self) -> None:
+        self.ticks_per_rotation = [
             self.__calculate_future_dial_value(rotation)
             for rotation in self.rotations
         ]
+        self.parsed = True
+        
+    def solve(self, part: Literal[1, 2]):
+        if not self.parsed:
+            self.__parse()
 
-        return tick_numbers.count(0)
+        match part:
+            case 1:
+                return self.ticks_per_rotation.count(0)
+            
+            case 2:
+                return self.pass_by_zero_count
 
 def run():
     file = get_input_file_from_script_file(__file__)
     solver = Solver(file)
 
-    solution_print(1, solver.solve())
+    solution_print(1, solver.solve(1))
+    solution_print(2, solver.solve(2))
 
 if __name__ == "__main__":
     run()
